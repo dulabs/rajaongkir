6aad2075ea39a5311b49a8c94ac4a2d293bab720
Abdul Ibad <ibad_abdul@yahoo.com.sg>
2016-07-19 16:57:37 +0700

    add method getServices to get price

--- src/App/Cost.php
+++ src/App/Cost.php
@@ -3,19 +3,38 @@
 namespace Dulabs\RajaOngkir\App;
 
 class Cost extends Api {
+
 	public function __construct($attributes){
+
 		parent::__construct();
+
+		$this->setAttributes($attributes);
+	}
+
+	/**
+	 * Request attribute paramters
+	 * @var array
+	 */
+	protected $attributes;
+
+	public function setAttributes($attributes)
+	{
+		$this->attributes = $attributes;
+	}
+
+	public function getServices()
+	{
 
 		$this->overWriteOptions = [
 			CURLOPT_CUSTOMREQUEST => "POST",
-			CURLOPT_POSTFIELDS => http_build_query($attributes),
+			CURLOPT_POSTFIELDS => http_build_query($this->attributes),
 			CURLOPT_HTTPHEADER => [
 				"content-type: application/x-www-form-urlencoded",
 	    		"key: ".$this->apiKey
 			]
 		];
 
-		$this->GetData();
+		return $this->GetData();
 	}
 
 	protected $method = "cost";
